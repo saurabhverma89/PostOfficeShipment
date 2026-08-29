ee7fdcc991d8cbc32c7072f98d4b679771339311
ssaurabh89 <ssaurabh.verma@gmail.com>
2026-08-30 00:49:00 +0530

    Added Docker Compose environment for api and PostgreSQL

--- src/PostOfficeShipment.Api/Program.cs
+++ src/PostOfficeShipment.Api/Program.cs
@@ -5,55 +5,73 @@ using PostOfficeShipment.Application.Services;
 using PostOfficeShipment.Infrastructure.Data;
 using PostOfficeShipment.Infrastructure.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ShipmentDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("ShipmentDb")));
-
-builder.Services.AddScoped<IShipmentRepository, ShipmentRepository>();
-builder.Services.AddScoped<IPostOfficeRepository, PostOfficeRepository>();
-
-builder.Services.AddScoped<IShipmentService, ShipmentService>();
-builder.Services.AddScoped<IPostOfficeService, PostOfficeService>();
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-builder.Services.AddCors(options =>
+namespace PostOfficeShipment.Api
 {
-    options.AddPolicy("Frontend", policy =>
+    public static class Program
     {
-        policy
-        .WithOrigins("http://localhost:5173")
-        .AllowAnyHeader()
-        .AllowAnyMethod();
-    });
-});
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+            // Add services to the container.
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+            builder.Services.AddDbContext<ShipmentDbContext>(options =>
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("ShipmentDb")));
+
+            builder.Services.AddScoped<IShipmentRepository, ShipmentRepository>();
+            builder.Services.AddScoped<IPostOfficeRepository, PostOfficeRepository>();
+
+            builder.Services.AddScoped<IShipmentService, ShipmentService>();
+            builder.Services.AddScoped<IPostOfficeService, PostOfficeService>();
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+            builder.Services.AddProblemDetails();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Frontend", policy =>
+                {
+                    policy
+                    .WithOrigins("http://localhost:5173")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider
+                .GetRequiredService<ShipmentDbContext>();
+
+                dbContext.Database.Migrate();
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                app.UseHttpsRedirection();
+            }
+
+            app.UseExceptionHandler();
+
+            app.UseCors("Frontend");
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseExceptionHandler();
-app.UseHttpsRedirection();
-
-app.UseCors("Frontend");
-app.MapControllers();
-
-app.Run();
